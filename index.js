e46d8805f9e653510e14e3f77dfaa0b265e4b84b
lebrancconvas <lebrancconvas@gmail.com>
2022-04-04 14:35:04 +0700

    :sparkles: Success Feature.

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ async function getFriendship() {
   let msg = "Hooray! You and our chatbot are friend."
   const friend = await liff.getFriendship()
   if (!friend.friendFlag) {
-     msg = "<a href=\"https://line.me/R/ti/p/@BOT-ID\">Follow our chatbot here!</a>"
+     msg = "<a href=\"https://line.me/R/ti/p/@651klmwp\">Follow our chatbot here!</a>"
   }
   friendShip.innerHTML = msg;
 }
